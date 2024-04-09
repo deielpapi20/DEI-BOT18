071ba4371297390aaea2d06f5aa08ceefb4c01b6
deielpapi20 <165846927+deielpapi20@users.noreply.github.com>
2024-04-09 00:27:50 -0500

    owner-noetiquetar.js
    Signed-off-by: deielpapi20 <165846927+deielpapi20@users.noreply.github.com>

--- plugins/owner-noetiquetar.js
+++ plugins/owner-noetiquetar.js
@@ -1,9 +1,9 @@
 let handler = async (m, {conn, text}) => {
   let name = await conn.getName(m.sender);
 
-conn.reply(m.chat, `*[ 💖 ️] No etiquetes a mi owner, si es algo urgente contacta con el a su chat privado para mas informacion*` ,m, { contextInfo:{ externalAdReply: {title: '𝕷𝖔𝖇𝖔-𝕭𝖔𝖙-𝕸𝕯', body: 'ᴇsᴄʀɪʙɪʀʟᴇ ᴀᴘʟᴀsᴛᴀɴᴛᴏ ᴀǫᴜɪ', sourceUrl: 'https://wa.me/50558124470'}}})
+conn.reply(m.chat, `*[ 💖 ️] No etiquetes a mi owner, si es algo urgente contacta con el a su chat privado para mas informacion*` ,m, { contextInfo:{ externalAdReply: {title: '𝕷𝖔𝖇𝖔-𝕭𝖔𝖙-𝕸𝕯', body: 'ᴇsᴄʀɪʙɪʀʟᴇ ᴀᴘʟᴀsᴛᴀɴᴛᴏ ᴀǫᴜɪ', sourceUrl: 'https://wa.me/+51975661139'}}})
 }
-handler.customPrefix = /@50558124470/i;
+handler.customPrefix = /@+51975661139/i;
 handler.command = new RegExp();
 
 export default handler;
